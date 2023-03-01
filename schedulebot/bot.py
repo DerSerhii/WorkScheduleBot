5959eceb995adf9a3832c61de9769b2c0aa976c7
serhii <proektssd@gmail.com>
2023-03-01 14:01:47 +0200

    refactor(command): added Enum for command

--- schedulebot/bot.py
+++ schedulebot/bot.py
@@ -1,19 +1,20 @@
 import logging
 from aiogram import Bot, Dispatcher, executor
 
-import settings
+from settings import TELEGRAM_TOKEN, Handler
 from schedulebot import handlers
 
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=settings.TELEGRAM_TOKEN)
+bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
 
-dp.register_message_handler(handlers.start, commands=['start'])
-dp.register_message_handler(handlers.client, commands=['client'])
-dp.register_message_handler(handlers.moderator, commands=['moderator'])
-dp.register_message_handler(handlers.superuser, commands=['superuser'])
+dp.register_message_handler(handlers.start, commands=[Handler.START.value])
+dp.register_message_handler(handlers.client, commands=[Handler.CLIENT.value])
+dp.register_message_handler(handlers.moderator, commands=[Handler.MODERATOR.value])
+dp.register_message_handler(handlers.superuser, commands=[Handler.SUPERUSER.value])
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
