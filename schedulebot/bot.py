d74bd6d213b7e9d20f15c88a2725609659d321dc
serhii <proektssd@gmail.com>
2023-03-01 17:53:32 +0200

    refactor(register): register_message_handler

--- schedulebot/bot.py
+++ schedulebot/bot.py
@@ -5,16 +5,26 @@ from settings import TELEGRAM_TOKEN, Handler
 from schedulebot import handlers
 
 
+COMMAND_HANDLERS = {
+    Handler.START.value: handlers.start,
+    Handler.CLIENT.value: handlers.client,
+    Handler.MODERATOR.value: handlers.moderator,
+    Handler.SUPERUSER.value: handlers.superuser,
+}
+
+
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=TELEGRAM_TOKEN)
-dp = Dispatcher(bot)
 
-dp.register_message_handler(handlers.start, commands=[Handler.START.value])
-dp.register_message_handler(handlers.client, commands=[Handler.CLIENT.value])
-dp.register_message_handler(handlers.moderator, commands=[Handler.MODERATOR.value])
-dp.register_message_handler(handlers.superuser, commands=[Handler.SUPERUSER.value])
+def main() -> None:
+    bot = Bot(token=TELEGRAM_TOKEN)
+    dp = Dispatcher(bot)
+
+    for command, handler in COMMAND_HANDLERS.items():
+        dp.register_message_handler(handler, commands=[command])
+
+    executor.start_polling(dp, skip_updates=True)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    main()
